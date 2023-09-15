5554ddd87f5b0f45abca922f766c7b9622051dd4
mayushii21 <122178787+mayushii21@users.noreply.github.com>
2023-09-15 16:53:36 +0300

    hooked up to dev program

--- bot.py
+++ bot.py
@@ -7,13 +7,15 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.filters.command import Command
 
+from dev import send_payload
+
 # Initialize the logger and load the .env file
 logging.basicConfig(level=logging.INFO)
 dotenv.load_dotenv()
 
 # Initialize Bot instance
 bot = Bot(token=os.getenv("BOT_TOKEN"), parse_mode=ParseMode.HTML)
-# # Dispatcher
+# Dispatcher
 dp = Dispatcher()
 
 
@@ -25,8 +27,20 @@ async def cmd_start(message: types.Message):
     )
 
 
-# And the run events dispatching
+# /get command
+@dp.message(Command("get"))
+async def get_top_ranked(message: types.Message):
+    for item in await send_payload():
+        url = item["img_url"]
+        await message.answer_photo(
+            photo=url,
+            caption=f"<a href='{item['page_url']}'>{item['title']}</a>\nAuthor: <a href='{item['author_url']}'>{item['author']}</a>\nTags: {item['tags']}",
+        )
+        await message.answer_document(document=url)
+
+
 async def main():
+    # Start the events dispatcher
     await dp.start_polling(bot)
 
 
