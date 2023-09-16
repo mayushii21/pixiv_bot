7292167c3ec307459b89001ba5a26d38669443b2
mayushii21 <122178787+mayushii21@users.noreply.github.com>
2023-09-16 19:13:56 +0300

    added scheduler cron job

--- src/bot.py
+++ src/bot.py
@@ -7,6 +7,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters.command import Command
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from dev import create_payload, populate_w_ids
 
@@ -19,6 +20,9 @@ bot = Bot(token=os.getenv("BOT_TOKEN"), parse_mode=ParseMode.HTML)
 # Dispatcher
 dp = Dispatcher()
 
+# Create a scheduler instance
+scheduler = AsyncIOScheduler()
+
 
 # /start command handler
 @dp.message(Command("start"))
@@ -28,7 +32,16 @@ async def cmd_start(message: types.Message):
     )
 
 
-# /get command
+async def send_artwork(item, url):
+    await bot.send_photo(
+        os.getenv("CHANNEL_ID"),
+        photo=url,
+        caption=f"<a href='{item['page_url']}'>{item['title']}</a>\nAuthor: <a href='{item['author_url']}'>{item['author']}</a>\nTags: {item['tags']}",
+    )
+    await bot.send_document(os.getenv("CHANNEL_ID"), document=url)
+
+
+# /get command in case cron fails
 @dp.message(Command("get"))
 async def send_payload(message: types.Message):
     payload, new_sfw_ids, nsfw_ids = await create_payload()
@@ -36,25 +49,39 @@ async def send_payload(message: types.Message):
     tries = 0
     while new_sfw_ids and tries < 3:
         for item in payload:
+            # Skip already sent artworks
             if item["artwork_id"] in sent_sfw_ids:
                 continue
             url = item["img_url"]
             try:
-                await message.answer_photo(
-                    photo=url,
-                    caption=f"<a href='{item['page_url']}'>{item['title']}</a>\nAuthor: <a href='{item['author_url']}'>{item['author']}</a>\nTags: {item['tags']}",
-                )
-                await message.answer_document(document=url)
+                await send_artwork(item, url)
                 sent_sfw_ids.add(item["artwork_id"])
+                # Flood control prevention
+                await asyncio.sleep(6)
             except TelegramBadRequest:
                 pass
         tries += 1
         new_sfw_ids.difference_update(sent_sfw_ids)
-
+        # Wait between retries
+        await asyncio.sleep(10)
+    # Populate db and update set with processed id's
     populate_w_ids(sent_sfw_ids, nsfw_ids)
 
 
 async def main():
+    # Schedule the send_payload function to run daily at 12:05 noon (GMT+9)
+    scheduler.add_job(
+        send_payload,
+        "cron",
+        hour=12,  # Set the hour to 12 (noon)
+        minute=5,  # Set the minute to 5
+        second=0,  # Set the second to 0
+        timezone="Asia/Tokyo",  # Set the timezone to GMT+9 (Asia/Tokyo)
+        args=(None,),
+    )
+    # Start the scheduler
+    scheduler.start()
+
     # Start the events dispatcher
     await dp.start_polling(bot)
 
